b8356f8796db0d92feffca28393ef7c8db254814
Can Toraman <toraman@usc.edu>
2022-11-08 18:23:27 -0800

    includes

---
+++ chapter-7/4_bringing_paths_into_scope_with_use/src/main.rs
@@ -0,0 +1,10 @@
+use std::collections::HashMap;
+use std::collections::*;
+use std::fmt;
+use std::io::Result as IoResult;
+use std::io::{self, Write}; // take io as io::, and io::Write as Write // glob op
+
+fn main() {
+    let mut map = HashMap::new();
+    map.insert(1, 2);
+}
